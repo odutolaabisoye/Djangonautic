188b916e0655f19f6fd7ac08eda615d2181835f2
odutolaabisoye <odutolaabisoye@gmail.com>
2020-04-26 02:23:41 +0100

    Named Url

--- articles/urls.py
+++ articles/urls.py
@@ -2,8 +2,10 @@ from django.conf.urls import url
 from django.contrib import admin
 from . import views
 
+app_name = 'articles'
+
 urlpatterns = [
-    url(r'^$', views.article_list),
-    url(r'^(?P<slug>[\w-]+)/$', views.articles_details),
+    url(r'^$', views.article_list, name="list"),
+    url(r'^(?P<slug>[\w-]+)/$', views.articles_details, name="detail"),
   #  url(r'^$',)
 ]
