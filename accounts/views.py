b5ecef6f32a99e32e6dfa781edc1e089c861e083
odutolaabisoye <odutolaabisoye@gmail.com>
2020-04-27 04:08:50 +0100

    login

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 # Create your views here.
 def signup_view(request):
     if request.method == 'POST':
@@ -13,3 +13,12 @@ def signup_view(request):
     else:
         form = UserCreationForm()
     return render(request, 'accounts/signup.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            return redirect('articles:list')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', {'form': form })
