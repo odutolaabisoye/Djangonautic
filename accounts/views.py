6b1efc4db5eb02e87d9b73886744258fe17128da
odutolaabisoye <odutolaabisoye@gmail.com>
2020-04-27 03:26:39 +0100

    user signup

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 def signup_view(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:list')
+    else:
+        form = UserCreationForm()
     return render(request, 'accounts/signup.html', {'form': form})
